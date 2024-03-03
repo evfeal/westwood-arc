f8646f1ff0b4344d8e4f6ae0056eefd2cde131bc
Evan Alvarez <evanalvarez@tutanota.com>
2024-03-03 14:13:01 -0600

    did some stuff

--- components/custom/NavMobile.tsx
+++ components/custom/NavMobile.tsx
@@ -1,38 +1,38 @@
 import Link from "next/link";
 import { routes } from "@/utils/routes";
 import {
-    Sheet,
-    SheetContent,
-    SheetHeader,
-    SheetTitle,
-    SheetTrigger,
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
 } from "@/components/ui/sheet";
 
 import { Menu } from "lucide-react";
 
 export default function NavbarMobile() {
-    return (
-        <>
-            <div className="visible sm:invisible flex flex-row row-start-1 row-end-1 col-start-1 col-end-1 text-2xl items-center justify-end font-bold">
-                <Sheet>
-                    <SheetTrigger aria-label="Menu Button">
-                        <SheetContent>
-                            <SheetHeader className="gap-4 py-24">
-                                {routes.map((route) => {
-                                    return (
-                                        <SheetTitle className="flex flex-col">
-                                            <Link href={route.href} className="text-3xl">
-                                                {route.title}
-                                            </Link>
-                                        </SheetTitle>
-                                    );
-                                })}
-                            </SheetHeader>
-                        </SheetContent>
-                        <Menu size={32} />
-                    </SheetTrigger>
-                </Sheet>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="visible sm:invisible flex flex-row row-start-1 row-end-1 col-start-1 col-end-1 text-2xl items-center justify-end font-bold">
+        <Sheet>
+          <SheetTrigger aria-label="Menu Button">
+            <SheetContent>
+              <SheetHeader className="gap-4 py-24">
+                {routes.map((route) => {
+                  return (
+                    <SheetTitle className="flex flex-col" key={route.id}>
+                      <Link href={route.href} className="text-3xl">
+                        {route.title}
+                      </Link>
+                    </SheetTitle>
+                  );
+                })}
+              </SheetHeader>
+            </SheetContent>
+            <Menu size={32} />
+          </SheetTrigger>
+        </Sheet>
+      </div>
+    </>
+  );
 }
--- utils/routes.tsx
+++ utils/routes.tsx
@@ -1,18 +1,22 @@
 export const routes = [
-    {
-        title: "Home",
-        href: "/",
-    },
-    {
-        title: "About",
-        href: "/about",
-    },
-    {
-        title: "Programs",
-        href: "/programs",
-    },
-    {
-        title: "Gallery",
-        href: "/gallery",
-    },
+  {
+    id: 0,
+    title: "Home",
+    href: "/",
+  },
+  {
+    id: 1,
+    title: "About",
+    href: "/about",
+  },
+  {
+    id: 2,
+    title: "Programs",
+    href: "/programs",
+  },
+  {
+    id: 3,
+    title: "Gallery",
+    href: "/gallery",
+  },
 ];
