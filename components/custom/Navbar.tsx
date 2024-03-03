085180352475831cf8b93a92ee27f036a5eaf436
Evan Alvarez <evanalvarez@tutanota.com>
2024-03-03 10:04:44 -0600

    changed the logo to 'priority' loading

--- components/custom/Navbar.tsx
+++ components/custom/Navbar.tsx
@@ -5,28 +5,22 @@ import NavbarDesktop from "@/components/custom/NavDesktop";
 import NavbarMobile from "@/components/custom/NavMobile";
 
 export default function Navbar() {
-    {
-        return (
-            <header className="grid grid-cols-1 px-4 py-6 bg-opacity-0">
-                <Link
-                    className="flex flex-row row-start-1 row-end-1 col-start-1 col-end-1 text-2xl items-center font-bold text-gray-100"
-                    href="#"
-                >
-                    <Image
-                        src={arcLogo}
-                        alt="Cool Logo for ARC"
-                        width={100}
-                        height={100}
-                        loading="lazy"
-                    />
-                    <h1 className="text-black dark:text-white invisible lg:visible">
-                        Westwood ARC
-                    </h1>
-                    <span className="sr-only">ARC Logo</span>
-                </Link>
-                <NavbarDesktop />
-                <NavbarMobile />
-            </header>
-        );
-    }
+  {
+    return (
+      <header className="grid grid-cols-1 px-4 py-6 bg-opacity-0">
+        <Link
+          className="flex flex-row row-start-1 row-end-1 col-start-1 col-end-1 text-2xl items-center font-bold text-gray-100"
+          href="#"
+        >
+          <Image src={arcLogo} alt="Cool Logo for ARC" priority />
+          <h1 className="text-black dark:text-white invisible lg:visible">
+            Westwood ARC
+          </h1>
+          <span className="sr-only">ARC Logo</span>
+        </Link>
+        <NavbarDesktop />
+        <NavbarMobile />
+      </header>
+    );
+  }
 }
