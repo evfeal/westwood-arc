5faacfd8f7f4940aa7d034b05b2ad00b7297ff79
Evan Alvarez <evanalvarez@tutanota.com>
2024-03-03 13:17:54 -0600

    added vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,28 @@
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import Navbar from "@/components/custom/Navbar";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const opensans = Open_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "Westwood ARC",
-    description: "The official website of Westwood ARC!",
+  title: "Westwood ARC",
+  description: "The official website of Westwood ARC!",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en" suppressHydrationWarning>
-            <body className={opensans.className}>
-                <Navbar />
-                {children}
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={opensans.className}>
+        <SpeedInsights />
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
 }
