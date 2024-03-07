22033161bfe17e2d09467006ba9441ea5a415efd
Evan Alvarez <evanalvarez@tutanota.com>
2024-03-06 23:38:48 -0600

    added the favicons!

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,21 +6,34 @@ import "./globals.css";
 const opensans = Open_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Westwood ARC",
-  description: "The official website of Westwood ARC!",
+    title: "Westwood ARC",
+    description: "The official website of Westwood ARC!",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`${opensans.className}`}>
-          <Navbar />
-          {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" suppressHydrationWarning>
+            <link rel="icon" href="/favicon.ico" sizes="any" />
+            <link
+                rel="icon"
+                href="/icon?<generated>"
+                type="image/<generated>"
+                sizes="<generated>"
+            />
+            <link
+                rel="apple-touch-icon"
+                href="/apple-icon?<generated>"
+                type="image/<generated>"
+                sizes="<generated>"
+            />
+            <body className={`${opensans.className}`}>
+                <Navbar />
+                {children}
+            </body>
+        </html>
+    );
 }
