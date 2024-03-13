0491b993c33601bcb4ee3174dffcddfa7f097e94
Evan Alvarez <evanalvarez@tutanota.com>
2024-03-13 17:37:31 -0500

    did i fix the mobile menu?

--- components/custom/NavMobile.tsx
+++ components/custom/NavMobile.tsx
@@ -4,47 +4,47 @@ import { useState } from "react";
 import Link from "next/link";
 import { routes } from "@/utils/routes";
 import {
-  Sheet,
-  SheetContent,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
+    Sheet,
+    SheetContent,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
 } from "@/components/ui/sheet";
 
 import { Menu } from "lucide-react";
 
 export default function NavbarMobile() {
-  const [showingMenu, setShowingMenu] = useState(false);
+    const [showingMenu, setShowingMenu] = useState(false);
 
-  function HandleClick() {
-    setShowingMenu(!showingMenu);
-  }
+    function HandleClick() {
+        setShowingMenu(!showingMenu);
+    }
 
-  return (
-    <>
-      <div className="visible sm:invisible flex flex-row row-start-1 row-end-1 col-start-1 col-end-1 text-2xl items-center justify-end font-bold">
-        <Sheet>
-          <SheetTrigger aria-label="Menu Button" onClick={HandleClick}>
-            <Menu size={32} />
-          </SheetTrigger>
-          <SheetContent>
-            <SheetHeader className="gap-4 py-24">
-              {routes.map((route) => {
-                return (
-                  <SheetTitle className="flex flex-col" key={route.id}>
-                    <Link
-                      href={route.href}
-                      className="text-3xl hover:underline"
-                    >
-                      {route.title}
-                    </Link>
-                  </SheetTitle>
-                );
-              })}
-            </SheetHeader>
-          </SheetContent>
-        </Sheet>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="visible sm:invisible flex flex-row row-start-1 row-end-1 col-start-1 col-end-1 text-2xl items-center justify-end font-bold">
+                <Sheet>
+                    <SheetTrigger aria-label="Menu Button" onClick={HandleClick}>
+                        <Menu size={32} />
+                    </SheetTrigger>
+                    <SheetContent>
+                        <SheetHeader className="flex flex-col bg-red-400 gap-4 py-24">
+                            {routes.map((route) => {
+                                return (
+                                    <SheetTitle key={route.id}>
+                                        <Link
+                                            href={route.href}
+                                            className="text-3xl hover:underline"
+                                        >
+                                            {route.title}
+                                        </Link>
+                                    </SheetTitle>
+                                );
+                            })}
+                        </SheetHeader>
+                    </SheetContent>
+                </Sheet>
+            </div>
+        </>
+    );
 }
