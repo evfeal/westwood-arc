99d129e8e4cd60d0b3973a15f72d9ba74b91eb6e
Evan Alvarez <evanalvarez@tutanota.com>
2024-06-11 12:37:23 -0500

    trimmed down components/ui/sheet.tsx

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -88,20 +88,6 @@ const SheetHeader = ({
 );
 SheetHeader.displayName = "SheetHeader";
 
-const SheetFooter = ({
-    className,
-    ...props
-}: React.HTMLAttributes<HTMLDivElement>) => (
-    <div
-        className={cn(
-            "flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2",
-            className,
-        )}
-        {...props}
-    />
-);
-SheetFooter.displayName = "SheetFooter";
-
 const SheetTitle = React.forwardRef<
     React.ElementRef<typeof SheetPrimitive.Title>,
     React.ComponentPropsWithoutRef<typeof SheetPrimitive.Title>
@@ -114,18 +100,6 @@ const SheetTitle = React.forwardRef<
 ));
 SheetTitle.displayName = SheetPrimitive.Title.displayName;
 
-const SheetDescription = React.forwardRef<
-    React.ElementRef<typeof SheetPrimitive.Description>,
-    React.ComponentPropsWithoutRef<typeof SheetPrimitive.Description>
->(({ className, ...props }, ref) => (
-    <SheetPrimitive.Description
-        ref={ref}
-        className={cn("text-sm text-muted-foreground", className)}
-        {...props}
-    />
-));
-SheetDescription.displayName = SheetPrimitive.Description.displayName;
-
 export {
     Sheet,
     SheetPortal,
@@ -134,7 +108,5 @@ export {
     SheetClose,
     SheetContent,
     SheetHeader,
-    SheetFooter,
     SheetTitle,
-    SheetDescription,
 };
